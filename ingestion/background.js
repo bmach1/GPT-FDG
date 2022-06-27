486b748a6bdb371e91edcdc583300b2976f96add
Brian <bmachado@live.ca>
2022-06-26 23:19:19 -0400

    zoinL

--- ingestion/background.js
+++ ingestion/background.js
@@ -1,9 +1,16 @@
 let activeTabId, lastUrl, lastTitle;
 
+var db = new PouchDB('todos');
+var remoteCouch = false;
+
 function getTabInfo(tabId) {
   chrome.tabs.get(tabId, function(tab) {
     if(lastUrl != tab.url || lastTitle != tab.title)
       console.log(lastUrl = tab.url, lastTitle = tab.title);
+
+      chrome.storage.sync.set({key: value}, function() {
+        console.log('Value is set to ' + value);
+      });
   });
 }
 
@@ -18,22 +25,4 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 });
 
 
-async function getCurrentTab() {
-  let queryOptions = { active: true, lastFocusedWindow: true };
-  // `tab` will either be a `tabs.Tab` instance or `undefined`.
-  let [tab] = await chrome.tabs.query(queryOptions);
-  return tab;
-}
-
-// When the button is clicked, inject setPageBackgroundColor into current page
-changeColor.addEventListener("click", async () => {
-  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
-});
-
-chrome.storage.sync.set({key: value}, function() {
-  console.log('Value is set to ' + value);
-});
-
-console.log(getCurrentTab())
 
